12770c0b3ccb0e369901a7bc4004902a930ab706
ShirishaVemula <31745286+ShirishaVemula@users.noreply.github.com>
2017-09-08 16:26:19 +0530

    added files

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-public class Main {
+ public class Main {
 
 	public static void main(String[] args) {
 		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		Scanner s1=new Scanner(System.in);
+		int id=s1.nextInt();
+		String fullname=s1.next();
+		String birthDate=s1.next();
+		double avgMark=s1.nextDouble();
+		Student s1=new Student( id, fullname, birthDate, avgMark );
 	}
 
 }
